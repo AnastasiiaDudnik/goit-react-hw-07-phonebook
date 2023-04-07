c0106fa0e25b7746f51bab1da715c17120cb7083
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-07 20:51:15 +0300

    temporary commented components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,31 @@
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { getContacts } from 'redux/selectors';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { ContactList } from './ContactList/ContactList';
+// import { Filter } from './Filter/Filter';
 import { Layout } from './Layout/Layout';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const { items, isLoading, error } = useSelector(getContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts);
+  }, [dispatch]);
+
   return (
     <Layout>
+      {isLoading && <p>Loading contacts...</p>}
+      {error && <p>{error}</p>}
+      {items && <p>JSON.stringify(items, null, 2)</p>}
       <h1>Phonebook</h1>
-      <ContactForm />
+      {/* <ContactForm /> */}
 
       <h2>Contacts</h2>
-      <Filter />
-      <ContactList />
+      {/* <Filter /> */}
+      {/* <ContactList /> */}
     </Layout>
   );
 };
