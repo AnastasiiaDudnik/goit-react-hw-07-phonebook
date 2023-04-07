0bf556830b9f0b849297116cc6624b15a38252a8
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-07 21:38:29 +0300

    use selectors in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
-import { getContacts } from 'redux/selectors';
+import { getError, getIsLoading } from 'redux/selectors';
 // import { ContactForm } from './ContactForm/ContactForm';
 // import { ContactList } from './ContactList/ContactList';
 // import { Filter } from './Filter/Filter';
@@ -9,7 +9,8 @@ import { Layout } from './Layout/Layout';
 
 export const App = () => {
   const dispatch = useDispatch();
-  const { items, isLoading, error } = useSelector(getContacts);
+  const error = useSelector(getError);
+  const isLoading = useSelector(getIsLoading);
 
   useEffect(() => {
     dispatch(fetchContacts);
@@ -17,13 +18,11 @@ export const App = () => {
 
   return (
     <Layout>
-      {isLoading && <p>Loading contacts...</p>}
-      {error && <p>{error}</p>}
-      {items && <p>JSON.stringify(items, null, 2)</p>}
       <h1>Phonebook</h1>
       {/* <ContactForm /> */}
 
       <h2>Contacts</h2>
+      {isLoading && !error && <p>Loading contacts...</p>}
       {/* <Filter /> */}
       {/* <ContactList /> */}
     </Layout>
